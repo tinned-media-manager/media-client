b4694831c26c47a78cd5c20cb0c56c7a402adde2
dambergn <dambergn@gmail.com>
2018-01-17 20:57:46 -0800

    completed proof of concept pulling api data.

---
+++ app.js
@@ -0,0 +1,18 @@
+const apiKey = ''
+const images_uri = 'http://image.tmdb.org/t/p'
+$.ajax({
+    url: `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=Jack+Reacher`,
+    method: 'GET',
+    timeout: 2000
+  })
+  .then(
+    data => {
+      console.log(data);
+      $('#results').append(`<p>${data.results[0].title}</p>`);
+      $('#results').append(`<img src="${images_uri}/w300_and_h450_bestv2${data.results[0].poster_path}">`);
+
+    },
+    err => {
+      console.error(err)
+    }
+  )
