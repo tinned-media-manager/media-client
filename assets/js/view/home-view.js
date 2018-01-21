094cd5fac31981570196c3b9edc7d99cb18c16fe
Chris Lesesne <clesesne3@gmail.com>
2018-01-21 11:55:26 -0800

    added search by title functionality through search bar

--- assets/js/view/home-view.js
+++ assets/js/view/home-view.js
@@ -2,38 +2,43 @@ var app = app || {};
 
 (module => {
 
-    const homeView = {}
-    const images_uri = 'http://image.tmdb.org/t/p'
-    const img_size = '/w300_and_h450_bestv2'
+  const homeView = {}
+  const images_uri = 'http://image.tmdb.org/t/p'
+  const img_size = '/w300_and_h450_bestv2'
 
-    const $view = $('.recommended')
+  const $view = $('.recommended')
 
+  homeView.init = (Movie) => {
 
+    $('.page').hide()
 
-    homeView.init = (Movie) => {
+    $('.recommended').empty()
+    Movie.forEach(movie=> {
+      console.log(movie);
 
-        $('.page').hide()
-       
-        $('.recommended').empty()
-        Movie.forEach(movie=> {
-            console.log(movie);
- 
-            $('.recommended').append(`
-            <li class="rec-content" data-id="${movie.id}"> <h6>${movie.title}</h6><img src="${images_uri}${img_size}${movie.poster_path}"></li>
-            `)
-        })
- 
-      $('.recommended').on('click', 'li', (event) => {
+      $('.recommended').append(`
+        <li class="rec-content" data-id="${movie.id}"> <h6>${movie.title}</h6><img src="${images_uri}${img_size}${movie.poster_path}"></li>
+        `)
+    })
 
-        const id = $(event.target).data('id')
-        page('/api/movies/one/' + id)
+    $('#main-search').keypress(event => {
+      if(event.which === 13) {
+        event.preventDefault();
+        $('.search-results').remove();
+        let titleSearch = $('#main-search').val();
+        let urlTitle = titleSearch.split(' ').join('+');
+        app.Movie.searchAll(urlTitle);
+      }
+    })
 
-    })    
+    $('.recommended').on('click', 'li', (event) => {
+      const id = $(event.target).data('id')
+      page('/api/movies/one/' + id)
+    })
 
     $view.show()
+  }
 
-    }
-    
-    module.homeView = homeView
+  module.homeView = homeView
 
 }) (app)
