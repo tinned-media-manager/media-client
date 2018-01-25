0987ede89f8a175331053dd0df1dee6b48ea4c79
dambergn <dambergn@gmail.com>
2018-01-24 20:41:31 -0800

    Update main.js

--- assets/js/controller/main.js
+++ assets/js/controller/main.js
@@ -6,156 +6,206 @@
 
 (function ($) {
 
-  skel
-    .breakpoints({
-      xlarge: '(max-width: 1680px)',
-      large: '(max-width: 1280px)',
-      medium: '(max-width: 980px)',
-      small: '(max-width: 736px)',
-      xsmall: '(max-width: 480px)'
-    });
+	skel
+		.breakpoints({
+			xlarge: '(max-width: 1680px)',
+			large: '(max-width: 1280px)',
+			medium: '(max-width: 980px)',
+			small: '(max-width: 736px)',
+			xsmall: '(max-width: 480px)'
+		});
 
-  $(function () {
+	$(function () {
 
-    var $window = $(window),
-      $body = $('body'),
-      $wrapper = $('#page-wrapper'),
-      $banner = $('#banner'),
-      $header = $('#header');
+		var $window = $(window),
+			$body = $('body'),
+			$wrapper = $('#page-wrapper'),
+			$banner = $('#banner'),
+			$header = $('#header');
 
-    // Disable animations/transitions until the page has loaded.
-    $body.addClass('is-loading');
+		// Disable animations/transitions until the page has loaded.
+		$body.addClass('is-loading');
 
-    $window.on('load', function () {
-      window.setTimeout(function () {
-        $body.removeClass('is-loading');
-      }, 100);
-    });
+		$window.on('load', function () {
+			window.setTimeout(function () {
+				$body.removeClass('is-loading');
+			}, 100);
+		});
 
-    // Mobile?
-    if (skel.vars.mobile)
-      $body.addClass('is-mobile');
-    else
-      skel
-        .on('-medium !medium', function () {
-          $body.removeClass('is-mobile');
-        })
-        .on('+medium', function () {
-          $body.addClass('is-mobile');
-        });
+		// Mobile?
+		if (skel.vars.mobile)
+			$body.addClass('is-mobile');
+		else
+			skel
+				.on('-medium !medium', function () {
+					$body.removeClass('is-mobile');
+				})
+				.on('+medium', function () {
+					$body.addClass('is-mobile');
+				});
 
-    // Fix: Placeholder polyfill.
-    $('form').placeholder();
+		// Fix: Placeholder polyfill.
+		$('form').placeholder();
 
-    // Prioritize "important" elements on medium.
-    skel.on('+medium -medium', function () {
-      $.prioritize(
-        '.important\\28 medium\\29',
-        skel.breakpoint('medium').active
-      );
-    });
+		// Prioritize "important" elements on medium.
+		skel.on('+medium -medium', function () {
+			$.prioritize(
+				'.important\\28 medium\\29',
+				skel.breakpoint('medium').active
+			);
+		});
 
-    // Scrolly.
-    $('.scrolly')
-      .scrolly({
-        speed: 1500,
-        offset: $header.outerHeight()
-      });
+		// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500,
+				offset: $header.outerHeight()
+			});
 
-    // Menu.
-    $('#menu')
-      .append('<a href="#menu" class="close"></a>')
-      .appendTo($body)
-      .panel({
-        delay: 500,
-        hideOnClick: true,
-        hideOnSwipe: true,
-        resetScroll: true,
-        resetForms: true,
-        side: 'right',
-        target: $body,
-        visibleClass: 'is-menu-visible'
-      });
-    $('.search-btn').click(function (e) {
-      $('.page').hide();
-      e.preventDefault();
-      $('.search-results').remove();
-      let titleSearch = $('#main-search').val();
-      let urlTitle = titleSearch.split(' ').join('+');
-      app.Movie.searchAll(urlTitle);
-      page('/')
-      $('#search-results').show()
-      document.getElementById('one').style.backgroundColor = 'purple';
-      a.menueToggle;
-      return false;
-    }
-    );
+		// Menu.
+		$('#menu')
+			.append('<a href="#menu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
+		$('.search-btn').click(function (e) {
+			$('.page').hide();
+			e.preventDefault();
+			$('.search-results').remove();
+			let titleSearch = $('#main-search').val();
+			let urlTitle = titleSearch.split(' ').join('+');
+			app.Movie.searchAll(urlTitle);
+			page('/')
+			$('#search-results').show()
+			document.getElementById("one").style.backgroundColor = "purple";
+			a.menueToggle;
+			return false;
+		}
+		);
 
-    //fix this later
-
-
-    // $('.rec-content').click( function(event) {
-    // 	$('.page').hide();
-    // 	event.preventDefault();
-    // 	page(`/home/details/${$(event.target).data('id')}`)
-    // 	$('#media-details').show()
-    // 	document.getElementById("one").style.backgroundColor = "purple";
-    // 	a.menueToggle;
-    // 	return false;
+		//fix this later
 
 
-    $('#main-search').keyup(event => {
-      event.preventDefault()
-      if (event.keyCode === 13) {
-        // event.preventDefault()
-        $('.search-btn').click();
-      }
-    });
+		// $('.rec-content').click( function(event) {
+		// 	$('.page').hide();
+		// 	event.preventDefault();
+		// 	page(`/home/details/${$(event.target).data('id')}`)
+		// 	$('#media-details').show()
+		// 	document.getElementById("one").style.backgroundColor = "purple";
+		// 	a.menueToggle;
+		// 	return false; 
 
-    $('#sighnup').click(function (e) {
-      $('.page').hide();
-      page('/home/sighnup')
-      app.signUpView.init();
-      document.getElementById('one').style.backgroundColor = 'red';
-      a.menueToggle;
-      return false;
-    }
-    );
-    $('#login').click(function (e) {
-      $('.page').hide();
-      page('/home/login')
-      document.getElementById('one').style.backgroundColor = 'blue';
-      a.menueToggle;
-      return false;
-    }
-    );
-    $('#about-us').click(function (e) {
-      $('.page').hide();
-      page('/home/aboutus')
-      document.getElementById('one').style.backgroundColor = 'green';
-      a.menueToggle;
-      return false;
-    }
-    );
 
-    // Header.
-    if (skel.vars.IEVersion < 9)
-      $header.removeClass('alt');
+		$('#main-search').keyup(event => {
+			event.preventDefault()
+			if (event.keyCode === 13) {
+				// event.preventDefault()
+				$('.search-btn').click();
+			}
+			// document.getElementById("one").style.backgroundImage = "url('img.png')";
+		});
 
-    if ($banner.length > 0
+		$('#sighnup').click(function (e) {
+			$('.page').hide();
+			page('/home/sighnup')
+			document.getElementById("one").style.backgroundColor = "red";
+			// document.getElementById("one").style.backgroundImage = "url('img.png')";
+			a.menueToggle;
+			return false;
+		}
+		);
+		$('#login').click(function (e) {
+			$('.page').hide();
+			page('/home/login')
+			document.getElementById("one").style.backgroundColor = "blue";
+			// document.getElementById("one").style.backgroundImage = "url('img.png')";
+			a.menueToggle;
+			return false;
+		}
+		);
+		$('#about-us').click(function (e) {
+			$('.page').hide();
+			page('/home/aboutus')
+			document.getElementById("one").style.backgroundColor = "green";
+			requestAnimationFrame(step);
+			
+			$('.mynameis_audio')[0].play();
+			// document.getElementById("one").style.backgroundImage = "url('img.png')";
+			a.menueToggle;
+			return false;
+		}
+		);
+
+
+		// Header.
+		if (skel.vars.IEVersion < 9)
+			$header.removeClass('alt');
+
+		if ($banner.length > 0
 			&& $header.hasClass('alt')) {
 
-      $window.on('resize', function () { $window.trigger('scroll'); });
+			$window.on('resize', function () { $window.trigger('scroll'); });
 
-      $banner.scrollex({
-        bottom: $header.outerHeight() + 1,
-        terminate: function () { $header.removeClass('alt'); },
-        enter: function () { $header.addClass('alt'); },
-        leave: function () { $header.removeClass('alt'); }
-      });
+			$banner.scrollex({
+				bottom: $header.outerHeight() + 1,
+				terminate: function () { $header.removeClass('alt'); },
+				enter: function () { $header.addClass('alt'); },
+				leave: function () { $header.removeClass('alt'); }
+			});
 
-    }
+		}
 
-  });
 
+		// About us background
+
+		const $element = $('.mynameisinigomontoya');
+		const imagePath = 'https://webslinger.com.au/pens/mynameisinigomontoya/img';
+		const totalFrames = 239;
+		const animationDuration = 7966.348;
+		const timePerFrame = animationDuration / totalFrames;
+		let timeWhenLastUpdate;
+		let timeFromLastUpdate;
+		let frameNumber = 1;
+
+
+		function step(startTime) {
+			if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
+			timeFromLastUpdate = startTime - timeWhenLastUpdate;
+
+			if (timeFromLastUpdate > timePerFrame) {
+				$element.attr('src', imagePath + `/mynameisinigomontoya_${frameNumber}.jpg`);
+
+				timeWhenLastUpdate = startTime;
+
+				if (frameNumber >= totalFrames) {
+					frameNumber = 1;
+				} else {
+					frameNumber = frameNumber + 1;
+				}
+			}
+			requestAnimationFrame(step);
+		}
+
+		// $(document).ready(() => {
+		// 	for (var i = 1; i < totalFrames + 1; i++) {
+		// 		$('body').append(`<div id="preload-image-${i}" style="background-image: url('${imagePath}/mynameisinigomontoya_${i}.jpg');"></div>`);
+		// 	}
+		// });
+
+		// $(window).on('load', () => {
+		$('#about-us').on('click', () => {
+			// requestAnimationFrame(step);
+			// $('.mynameis_audio')[0].play();
+		});
+
+
+	});
 })(jQuery);
